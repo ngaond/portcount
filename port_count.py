c2dc9fc81b5356ee38ae7fafcd589801fdd9caa9
ngaond <90721945+ngaond@users.noreply.github.com>
2022-02-11 15:33:48 +0900

    Add files via upload

--- port_count.py
+++ port_count.py
@@ -4,33 +4,27 @@ if __name__ == '__main__':
     es = Elasticsearch("http://172.23.32.103:9200")
     count = 0
     deport = []
-    year = "2020"
+    year = "2021"
     month = ['.12', '.01', '.02', '.03', '.04', '.05', '.06', '.07', '.08', '.09', '.10', '.11', '.12']
-    n = 0
+    n = 7
     i = 0
-    while n < 13:
-        query = {'query': {'term': {'request': 'jsonrpc'}},
+    query = {'query': {'bool':{'must':[{'term': {'request': 'jsonrpc'}},{'term': {'source_ip': '206.81.15.31'}}]}},
+             'sort': {"@timestamp": {"order": "asc"}}}
+    result = es.search(index="xpot_accesslog-" + year + month[n], body=query, size=100000)
+    for log in result["hits"]["hits"]:
+        deport.append(log["_source"]["destination_port"])
+        deport = list(set(deport))
+        time = log["_source"]['@timestamp']
+        # ip = log["_source"]['source_ip']
+    while len(result["hits"]["hits"]) != 0:
+        query1 = {'query': {'bool':{'must':[{'term': {'request': 'jsonrpc'}},{'term': {'source_ip': '206.81.15.31'}}]}},
+                 'search_after': [time],
                  'sort': {"@timestamp": {"order": "asc"}},
                  }
-        result = es.search(index="xpot_accesslog-" + year + month[n], body=query, size=100000)
+        result = es.search(index="xpot_accesslog-" + year + month[n], body=query1, size=100000)
         for log in result["hits"]["hits"]:
             deport.append(log["_source"]["destination_port"])
             deport = list(set(deport))
             time = log["_source"]['@timestamp']
             # ip = log["_source"]['source_ip']
-        while len(result["hits"]["hits"]) != 0:
-            query = {'query': {'term': {'request': 'jsonrpc'}},
-                     'search_after': [time],
-                     'sort': {"@timestamp": {"order": "asc"}},
-                     }
-            result = es.search(index="xpot_accesslog-" + year + month[n], body=query, size=100000)
-            for log in result["hits"]["hits"]:
-                deport.append(log["_source"]["destination_port"])
-                deport = list(set(deport))
-                time = log["_source"]['@timestamp']
-                # ip = log["_source"]['source_ip']
-        n = n + 1
-        if n == 1:
-            year = "2021"
-        print(len(deport))
     print(len(deport))
