ae230ffa2965b8bd8545c1f72eb70ca08d462fcc
ngaond <90721945+ngaond@users.noreply.github.com>
2022-02-11 13:03:52 +0900

    Add files via upload

--- port_count.py
+++ port_count.py
@@ -10,35 +10,20 @@ if __name__ == '__main__':
     down = 1000
     n = 0
     i = 0
-    while i < 10:
-        log = ['1']
-        query = {'query':
-                     {'bool':
-                          {'must':
-                               {'term': {'request': 'jsonrpc'}},
-                           'must_not':
-                               [{'term': {'destination_port': '0'}}],
-                           'filter': {
-                               'range': {
-                                   'destination_port': {
-                                       "gte": up,
-                                       "lte": down
-                                   }
-                               }}
-                           }}}
-        while n < 13:
-            while len(log) != 0:
-                result = es.search(index="xpot_accesslog-" + year + month[n], body=query, size=1)
-                log = result["hits"]["hits"]
-                if len(log) != 0:
-                    deport.append(log[0]["_source"]["destination_port"])
-                    query['query']['bool']['must_not'].append(
-                        {'term': {'destination_port': log[0]["_source"]["destination_port"]}})
-                    count = count + 1
-            n = n + 1
-            if n == 1:
-                year = "2021"
-        i = i + 1
-        up = up + 1000
-        down = down + 1000
-    print(count)
+    log = ['1']
+    query = {'query':
+                 {'term': {'request': 'jsonrpc'}}
+             }
+    while n < 13:
+        while len(log) != 0:
+            result = es.search(index="xpot_accesslog-" + year + month[n], body=query, size=1)
+            log = result["hits"]["hits"]
+            if len(log) != 0:
+                deport.append(log[0]["_source"]["destination_port"])
+                deport = list(set(deport))
+                # query['query']['bool']['must_not'].append({'term': {'destination_port': log[0]["_source"]["destination_port"]}})
+                count = count + 1
+        n = n + 1
+        if n == 1:
+            year = "2021"
+    print(deport)
