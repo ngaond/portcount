d8a65c8614ae946aeb64106fd0a162f74528fa76
ngaond <90721945+ngaond@users.noreply.github.com>
2022-02-11 13:15:54 +0900

    Add files via upload

--- port_count.py
+++ port_count.py
@@ -6,24 +6,19 @@ if __name__ == '__main__':
     deport = []
     year = "2020"
     month = ['.12', '.01', '.02', '.03', '.04', '.05', '.06', '.07', '.08', '.09', '.10', '.11', '.12']
-    up = 1
-    down = 1000
     n = 0
     i = 0
-    log = ['1']
     query = {'query':
                  {'term': {'request': 'jsonrpc'}}
              }
     while n < 13:
-        while len(log) != 0:
-            result = es.search(index="xpot_accesslog-" + year + month[n], body=query, size=1)
-            log = result["hits"]["hits"]
-            if len(log) != 0:
-                deport.append(log[0]["_source"]["destination_port"])
-                deport = list(set(deport))
-                # query['query']['bool']['must_not'].append({'term': {'destination_port': log[0]["_source"]["destination_port"]}})
-                count = count + 1
+        result = es.search(index="xpot_accesslog-" + year + month[n], body=query, size=3800000)
+        for log in result["hits"]["hits"]:
+            deport.append(log["_source"]["destination_port"])
+            deport = list(set(deport))
+            # query['query']['bool']['must_not'].append({'term': {'destination_port': log[0]["_source"]["destination_port"]}})
         n = n + 1
         if n == 1:
             year = "2021"
-    print(deport)
+        print(len(deport))
+    print(len(deport))
